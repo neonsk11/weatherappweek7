65274ad7ad4ca9d7dab5e6e16580b3c9256fd5d4
neonsk11 <belenjacquet@gmail.com>
2025-06-23 02:05:53 -0300

    Add the basic search engine (no API)

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function handleSearchSumit (event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input")
+    let cityElement = document.querySelector("#weather-app-city")
+    cityElement.innerHTML =searchInput.value;
+}
+let searchFormElement= document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSumit);
