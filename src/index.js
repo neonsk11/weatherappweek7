0be3be968ce230160972e3926443395ae0605821
neonsk11 <belenjacquet@gmail.com>
2025-06-23 23:39:47 -0300

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
-function refreshWeather (response){
-    let temperatureElement = document.querySelector ("#temperature");
-    let temperature = response.data.temperature.current;
-    temperatureElement.innerHTML = Math.round(temperature);
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current; 
     let cityElement = document.querySelector("#weather-app-city");
-    cityElement.innerHTML = response.data.city;
-}
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
 
+    cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`; // Fixed property access
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;  
+    }
+
+    if (hours < 10) {  
+        hours = `0${hours}`; 
+    }
+
+    return `${day} ${hours}:${minutes}`; 
+}
 function searchCity(city){
  let apiKey = "7eo8fca0945e36ba8ac000cdt621a213";
  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
