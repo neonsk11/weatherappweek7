db736f5cef6340080fb71261216c9e28a969d7a1
neonsk11 <belenjacquet@gmail.com>
2025-06-24 00:15:17 -0300

    injected weather icon data

--- src/index.js
+++ src/index.js
@@ -7,13 +7,16 @@ function refreshWeather(response) {
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector ("#icon");
+
 
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-    windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`; // Fixed property access
+    windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`; 
     temperatureElement.innerHTML = Math.round(temperature);
+    iconElement.innerHTML =`<img src= ${response.data.condition.icon_url} class="weather-app-temperature-icon" >`
 }
 function formatDate(date) {
     let minutes = date.getMinutes();
@@ -34,7 +37,7 @@ function formatDate(date) {
     }
 
     if (hours < 10) {  
-        hours = `0${hours}`; 
+        hours = `0${hours}`; // it doesnt upload the hour idk why
     }
 
     return `${day} ${hours}:${minutes}`; 
